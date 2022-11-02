e4ea83d524cce5b93e35e034b11d45d1ca61ff14
Gilles Testard <g.testard@galites.net>
2022-11-02 10:16:08 +0100

    Start

--- src/routes/backend/+page.js
+++
@@ -1,6 +0,0 @@
-// export const load = async ({parent}) => {
-//     const {user} = await parent()
-//     // console.log('backend +page.js', user)
-
-//     return user
-// }
--- src/routes/backend/+page.svelte
+++ src/routes/backend/+page.svelte
@@ -1,14 +1,5 @@
 <script>
-    // export let data 
-    // // console.log('backend +page.svelte', data)
-    // let first_name = ''
-    // let last_name = ''
-    // let email = '' 
-    // if (data) {
-    //     first_name = data.user.first_name
-    //     last_name = data.user.last_name
-    //     email = data.user.email
-    // }
+
 </script>
 
 <h2 class="subtitle has-text-centered is-uppercase">dashboard</h2>
